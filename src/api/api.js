cf22123a12008f06574a31417c8c8acb8326dd6d
Mohammad Amin Tootoonchi <toutounchi.ma@gmail.com>
2020-09-16 09:56:45 +0430

    update URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // const url = 'http://localhost:8000';
-// const url = 'https://guarded-taiga-94931.herokuapp.com';
-const url = 'http://192.168.1.180:8000';
+// const url = 'http://192.168.1.180:8000';
+const url = 'https://daftarchak-backend.herokuapp.com';
 
 export function login(username, password, on_response) {
     axios.post(url + '/accounts/login', {
